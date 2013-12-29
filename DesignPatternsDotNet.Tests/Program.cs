03474dcbae65c9c0f80a93a1bfb470ef3028622c
Atanas Hristov <atanas.hristov@hotmail.com>
2013-12-28 19:36:39 -0600

    Initial

---
+++ DesignPatternsDotNet.Tests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDotNet.Tests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DoDemo();
+
+            Console.WriteLine("<Enter>");
+            Console.ReadLine();
+        }
+
+        public static void DoDemo()
+        {
+            Console.WriteLine("===================[Begin demo]=====================");
+
+
+            Console.WriteLine("===================[End demno]======================");
+        } 
+    }
+}
